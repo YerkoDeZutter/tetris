080bfb83c172ac0a28a1cfe0451fce4ae886fbd1
Yerko <yerkodezutter@gmail.com>
2019-01-16 11:23:15 +0100

    all block tipe's added

--- js/index.js
+++ js/index.js
@@ -8,26 +8,76 @@ var c = canvas.getContext("2d");
 c.fillStyle = "#000";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+function blockPicker(thisB){
+  if(thisB == 0){
+  return [
+    [1, 1, 1],
+    [0, 1, 0],
+    [0, 0, 0]
+  ]}
 
-const Ttetris = [
-  [1, 1, 1],
-  [0, 1, 0],
-  [0, 0, 0]
-]
+  if(thisB == 1){
+  return [
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 1, 0]
+  ]}
 
-const Ltetris = [
-  [1, 0, 0],
-  [1, 0, 0],
-  [1, 1, 0]
-]
+  if(thisB == 2){
+  return [
+    [0, 0, 1],
+    [0, 0, 1],
+    [0, 1, 1]
+  ]}
+
+  if(thisB == 3){
+  return [
+    [1, 1],
+    [1, 1]
+  ]}
+
+  if(thisB == 4){
+  return [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0]
+  ]}
+
+  if(thisB == 5){
+  return [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0]
+  ]}
+
+  if(thisB == 6){
+  return [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0]
+  ]}
+}
+
 
 let tetBlock = [Ttetris, Ltetris];
 
-tetBlock[0].forEach((row, y)=>{
-  row.forEach((block, x)=>{
-    if(block != 0){
-      c.fillStyle = "#fff";
-      c.fillRect(x*30, y*30, 30, 30);
-    }
+function DrawBlocks(tipe, place) {
+
+  tipe.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block != 0) {
+        c.fillStyle = "#fff";
+        c.fillRect(x * place.x, y * place.y, 30, 30);
+      }
+    })
   })
+
+}
+
+
+
+DrawBlocks(Ttetris, {
+  x: 30,
+  y: 30
 })
