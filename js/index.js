069b0c289a3be2e82ff6a2707b5463411ae41368
Yerko <yerkodezutter@gmail.com>
2019-01-16 16:48:41 +0100

    block go's down

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 var canvas = document.querySelector("canvas");
 
 canvas.width = (30 * 12);
-canvas.height = 700;
+canvas.height = (30 * 24);
 
 var c = canvas.getContext("2d");
 
 c.fillStyle = "#000";
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+// ---- TETRIS BLOCKEN ----
 
 function blockPicker(thisB){
   if(thisB == 0){
@@ -59,13 +61,19 @@ function blockPicker(thisB){
   ]}
 }
 
+
+// ----- DRAW CURRENT BLOCK -----
+
 function DrawBlocks(tipe, place) {
+
+  c.fillStyle = "#000";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
   tipe.forEach((row, y) => {
     row.forEach((block, x) => {
       if (block != 0) {
         c.fillStyle = "#fff";
-        c.fillRect(x * place.x, y * place.y, 30, 30);
+        c.fillRect((x * 30) + place.x, (y * 30) + place.y, 30, 30);
       }
     })
   })
@@ -73,6 +81,47 @@ function DrawBlocks(tipe, place) {
 }
 
 
+
+
+
+
+let DropCountDown = 0;
+let DropTime = 1000;
+
+let lastFrame = 0;
+
+//----- MAKE A NEW FRAME (above are time couters) -----
+function update(frame = 0){
+  const deltaFrame = frame - lastFrame;
+  lastFrame = frame;
+
+  DropCountDown = DropCountDown + deltaFrame
+
+  if(DropCountDown > DropTime){
+    CurBlock.position.y = CurBlock.position.y + 30;
+    DropCountDown = 0
+  }
+
+
+
+  //-----NEXT BLOCK-----
+  if(CurBlock.position.y == canvas.height){
+
+  }
+
+  // DrawBlocks(arena, {x: 0, y: 0})
+  DrawBlocks(CurBlock.block, CurBlock.position)
+  requestAnimationFrame(update)
+}
+
+
+
+
+
+
+
+
+//----- GET CURRENT BLOCK -----
 
 const CurBlock = {
   position: {x: 30, y: 30},
@@ -86,4 +135,4 @@ function newBlock(){
 
 newBlock()
 
-DrawBlocks(CurBlock.block, CurBlock.position)
+update()
