117731a3d4c96f816e71cc26b2b7364c53513409
Yerko <yerkodezutter@gmail.com>
2019-01-16 10:28:27 +0100

    make T and L block

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
 var canvas = document.querySelector("canvas");
 
-canvas.width = 300;
-canvas.height = 500;
+canvas.width = (30 * 12);
+canvas.height = 700;
 
 var c = canvas.getContext("2d");
 
 c.fillStyle = "#000";
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+const Ttetris = [
+  [1, 1, 1],
+  [0, 1, 0],
+  [0, 0, 0]
+]
+
+const Ltetris = [
+  [1, 0, 0],
+  [1, 0, 0],
+  [1, 1, 0]
+]
+
+let tetBlock = [Ttetris, Ltetris];
+
+tetBlock[0].forEach((row, y)=>{
+  row.forEach((block, x)=>{
+    if(block != 0){
+      c.fillStyle = "#fff";
+      c.fillRect(x*30, y*30, 30, 30);
+    }
+  })
+})
